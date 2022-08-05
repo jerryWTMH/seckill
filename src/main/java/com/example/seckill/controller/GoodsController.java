96797899e224a3504ba1adfe1f1b5236eea29bbc
Jerry Hsu <jerry1202.wtmh@gmail.com>
2022-08-05 20:17:12 +0800

    024 seckill count down in /goods/toDetail/

--- src/main/java/com/example/seckill/controller/GoodsController.java
+++ src/main/java/com/example/seckill/controller/GoodsController.java
@@ -3,6 +3,7 @@ package com.example.seckill.controller;
 import com.example.seckill.pojo.User;
 import com.example.seckill.service.IGoodsService;
 import com.example.seckill.service.IUserService;
+import com.example.seckill.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,7 @@ import org.thymeleaf.util.StringUtils;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/goods")
@@ -42,7 +44,28 @@ public class GoodsController {
     @RequestMapping("/toDetail/{goodsId}")
     public String toDetail(Model model, User user, @PathVariable Long goodsId){
         model.addAttribute("user", user);
-        model.addAttribute("goods", goodService.findGoodsVoByGoodsId(goodsId));
+        GoodsVo goodsVo = goodService.findGoodsVoByGoodsId(goodsId);
+        Date startDate = goodsVo.getStartDate();
+        Date endDate = goodsVo.getEndDate();
+        Date nowDate = new Date();
+        // Discount status
+        int secKillStatus = 0;
+        // Discount count down
+        int remainSeconds = 0;
+        // Discount haven't started
+        if(nowDate.before(startDate)){
+            remainSeconds = (int)(startDate.getTime() - nowDate.getTime()) / 1000;
+        } else if(nowDate.after(endDate)){
+            // Discount end
+            secKillStatus = 2;
+            remainSeconds = -1;
+        } else{
+            secKillStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("remainSeconds", remainSeconds);
+        model.addAttribute("secKillStatus", secKillStatus);
+        model.addAttribute("goods", goodsVo);
         return "goodsDetail";
     }
 }
