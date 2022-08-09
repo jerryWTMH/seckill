cca1f935412bbc130f807c8da30415d9670f7c3f
Jerry Hsu <jerry1202.wtmh@gmail.com>
2022-08-09 15:42:45 +0800

    031 use same user in jmeter to test

--- src/main/java/com/example/seckill/controller/UserController.java
+++ src/main/java/com/example/seckill/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.seckill.controller;
 
 
+import com.example.seckill.pojo.User;
+import com.example.seckill.vo.RespBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
@@ -14,5 +17,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-
+    /**
+     * User Information
+     * @param user
+     * @return
+     */
+    @RequestMapping("/info")
+    @ResponseBody
+    public RespBean info(User user){
+        return RespBean.success(user);
+    }
 }
