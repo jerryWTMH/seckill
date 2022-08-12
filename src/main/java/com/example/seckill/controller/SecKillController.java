192d8ab565e06b425ac49505958274c02e938a2d
Jerry Hsu <jerry1202.wtmh@gmail.com>
2022-08-12 12:00:08 +0800

    052 decrease the number in stock IN ADVANCE, and put the info of the goods into Redis IN ADVANCE

--- src/main/java/com/example/seckill/controller/SecKillController.java
+++ src/main/java/com/example/seckill/controller/SecKillController.java
@@ -10,17 +10,22 @@ import com.example.seckill.service.impl.SeckillOrderServiceImpl;
 import com.example.seckill.vo.GoodsVo;
 import com.example.seckill.vo.RespBean;
 import com.example.seckill.vo.RespBeanEnum;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/seckill")
-public class SecKillController {
+public class SecKillController implements InitializingBean {
     @Autowired
     private IGoodsService goodsService;
     @Autowired
@@ -45,21 +50,38 @@ public class SecKillController {
             return RespBean.error(RespBeanEnum.SESSION_ERROR);
         }
         model.addAttribute("user",user);
-        GoodsVo goods =goodsService.findGoodsVoByGoodsId(goodsId);
-        if(goods.getStockCount() < 1){
-            return RespBean.error(RespBeanEnum.EMPTY_STOCK);
-        }
         // Determine whether buy more than once
-        // The code below is MyBatis plus
-        //SeckillOrder seckillOrder = seckillOrderService.getOne(new QueryWrapper<SeckillOrder>().eq("user_id", user.getId()).eq("goods_id", goodsId));
+        ValueOperations valueOperations = redisTemplate.opsForValue();
         SeckillOrder seckillOrder = (SeckillOrder)redisTemplate.opsForValue().get("order:" + user.getId() + ":" + goodsId);
         if(seckillOrder != null){
             return RespBean.error(RespBeanEnum.REPEATE_ERROR);
         }
+        // decrease the number in stock IN ADVANCE
+        Long stock = valueOperations.decrement("seckillGoods" + goodsId); // This is atomic
+        if(stock < 0){
+            valueOperations.increment("seckillGoods:" + goodsId);
+            return RespBean.error(RespBeanEnum.EMPTY_STOCK);
+        }
         Order order = orderService.seckill(user, goods);
-        model.addAttribute("order", order);
-        model.addAttribute("goods", goods);
         return RespBean.success(order);
+
+
+//        GoodsVo goods =goodsService.findGoodsVoByGoodsId(goodsId);
+//        if(goods.getStockCount() < 1){
+//            return RespBean.error(RespBeanEnum.EMPTY_STOCK);
+//        }
+//        // Determine whether buy more than once
+//        // The code below is MyBatis plus
+//        //SeckillOrder seckillOrder = seckillOrderService.getOne(new QueryWrapper<SeckillOrder>().eq("user_id", user.getId()).eq("goods_id", goodsId));
+//        SeckillOrder seckillOrder = (SeckillOrder)redisTemplate.opsForValue().get("order:" + user.getId() + ":" + goodsId);
+//        if(seckillOrder != null){
+//            return RespBean.error(RespBeanEnum.REPEATE_ERROR);
+//        }
+//        Order order = orderService.seckill(user, goods);
+//        model.addAttribute("order", order);
+//        model.addAttribute("goods", goods);
+//        return RespBean.success(order);
+        return null;
     }
 
     @RequestMapping("/doSeckill2")
@@ -84,5 +106,20 @@ public class SecKillController {
         model.addAttribute("order", order);
         model.addAttribute("goods", goods);
         return "orderDetail";
+    }
+
+    /**
+     * System Initialization, Load the number of product in stock to Redis
+     * @throws Exception
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        List<GoodsVo> list = goodsService.findGoodsVo();
+        if(CollectionUtils.isEmpty(list)){
+            return;
+        }
+        list.forEach(goodsVo ->
+            redisTemplate.opsForValue().set("seckillGoods:" + goodsVo.getId(), goodsVo.getStockCount())
+        );
     }
 }
